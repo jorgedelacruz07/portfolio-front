fea5e2d57741951f2769dd5194c9ef041bd86583
Jorge de la Cruz Padilla <jdelacruzp7@gmail.com>
2022-11-08 12:36:11 -0500

    update content data

--- data/content.js
+++ data/content.js
@@ -1,4 +1,4 @@
 export const profile = {
   name: 'Jorge de la Cruz Padilla',
-  description: "I’m a software engineer with more than 5 years of experience. I have experience with Javascript (and Typescript), React.js, Node.js, Vue.js and PHP. I’ve worked with Express.js and Serverless as Node.js frameworks. I built a lot of websites with Next.js and others with Nuxt.js. Also I’ve worked with AWS products as DynamoDB, SQS, EC2 and S3. I like to work with Visual Code Studio, GitHub, Jira, Postman, Slack to help me to work better.",
+  description: "I’m a software engineer with more than 5 years of experience. I have experience with Javascript (and Typescript), Node.js and React.js. I’ve worked with Express.js, Koa.js and Serverless as Node.js frameworks. I built a lot of websites with Next.js and others with Nuxt.js. Also I’ve worked with AWS products as DynamoDB, SQS, EC2 and S3. In my first years of work, I worked with PHP (Laravel framework) and Vue.js. I like to work with Visual Code Studio, GitHub, Jira, Postman, Slack to help me to work better.",
 }
