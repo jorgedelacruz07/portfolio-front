169ab1b58a88c91715e969818223413fd83a2d0f
Jorge de la Cruz <jdelacruzp7@gmail.com>
2023-02-03 10:40:17 -0500

    update CV and add google analytics

--- data/content.js
+++ data/content.js
@@ -1,4 +1,5 @@
 export const profile = {
-  name: 'Jorge de la Cruz Padilla',
-  description: "I’m a software engineer with more than 5 years of experience. I have experience with Javascript (and Typescript), Node.js and React.js. I’ve worked with Express.js, Koa.js and Serverless as Node.js frameworks. I built a lot of websites with Next.js and others with Nuxt.js. Also I’ve worked with AWS products as DynamoDB, SQS, EC2 and S3. In my first years of work, I worked with PHP (Laravel framework) and Vue.js. I like to work with Visual Code Studio, GitHub, Jira, Postman, Slack to help me to work better.",
-}
+  name: "Jorge de la Cruz Padilla",
+  description:
+    "I’m a software engineer with more than 5 years of experience. I have experience with Javascript (and Typescript), Node.js and React.js. I’ve worked with Express.js, Koa.js and Serverless as Node.js frameworks. I built a lot of websites with Next.js and others with Nuxt.js. Also I’ve worked with AWS products as DynamoDB, SQS, EC2 and S3. In my first years of work, I worked with PHP (Laravel framework) and Vue.js. I like to work with Notion, Jira, Slack, Visual Code Studio, GitHub, Postman to help me to work better.",
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,35 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/Layout";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-4J8T4WP1S7"
+      />
+      <Script
+        id="google-analytics"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
 
